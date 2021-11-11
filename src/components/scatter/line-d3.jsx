4c8452748f0ec7ca4e64b3f38c35656a0ddb808d
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2021-11-10 18:31:34 -0600

    what is happening

--- src/components/scatter/line-d3.jsx
+++ src/components/scatter/line-d3.jsx
@@ -12,8 +12,43 @@ export default class LineD3{
         this.drawLine(this.element, this.data, this.xDomain, this.yDomain);
     }
 
-    drawLine(){
+    drawLine(el, shape, xD, yD){
+        let margin = {top: 10, right: 30, bottom: 30, left: 60}
+        const width = d3.select(el).node().parentNode.clientWidth - margin.left - margin.right;
+        const height = d3.select(el).node().parentNode.clientHeight - margin.top - margin.bottom;
 
+        let svg = d3.select(el)
+            .append('svg')
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scaleLinear()
+                .domain([xD.min, xD.max])
+                .range([ 0, width ]);
+        svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+          
+              // Add Y axis
+        var y = d3.scaleLinear()
+                .domain([yD.min, yD.max])
+                .range([ height, 0 ]);
+
+        svg.append("g")
+                .call(d3.axisLeft(y));
+
+        svg.append("path")
+                .datum(shape)
+                .attr("fill", "none")
+                .attr("stroke", "steelblue")
+                .attr("stroke-width", 1.5)
+                .attr("d", d3.line()
+                  .x(function(d) { return x(d.X) })
+                  .y(function(d) { return y(d.Y) })
+                  )
     }
 
 
